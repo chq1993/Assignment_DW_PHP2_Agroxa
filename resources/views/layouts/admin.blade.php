be06e1359429583b6ffdc02a5327c98a48def0b7
thanhson98 <dtsprogram98@gmail.com>
2020-07-31 18:29:45 +0700

    fix bug

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-  <title>Atlantic</title>
+  <title>Agroxa</title>
   <meta content="Đánh giá đối tượng" name="description" />
   <meta content="Themesbrand" name="author" />
   <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
