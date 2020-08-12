2b67a367907e6863333ab170286217687ee031da
hoangfunny28 <kizhoang28@users.noreply.github.com>
2020-08-12 18:37:28 +0700

    them table comment

---
+++ database/migrations/2020_08_11_182644_comment.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Comment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('comment')->unsigned()->nullable();
+            $table->foreign('comment')->references('id')->on('comment')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
